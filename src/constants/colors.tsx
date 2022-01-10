f3dc96f9e2b16bf9871c462f58339577aa12e990
Sean Y <seanyang@college.harvard.edu>
2022-01-10 04:00:56 -0600

    Fix colors and abstract away interpolation

---
+++ src/constants/colors.tsx
@@ -0,0 +1,8 @@
+const color = {
+  BACKGROUND: '#04182E',
+  FOREGROUND: '#0B223B',
+  MAIN: '#FFFFFF',
+  HIGHLIGHT: '#F2B628',
+}
+
+export default color
--- src/scenes/WindowScene.tsx
+++ src/scenes/WindowScene.tsx
@@ -2,9 +2,10 @@ import Window from '../components/Window'
 import SkyImage from '../media/sky.jpg'
 import styled from 'styled-components';
 import { Controller, Scene } from 'react-scrollmagic';
+import color from '../constants/colors';
 
 const InteriorWall = styled.div`
-  background-color: #0A213A;
+  background-color: ${color.BACKGROUND};
   width:100%;
   height:100vh;
   display:flex;
@@ -28,6 +29,18 @@ const SkyImageComponent = styled.div`
   background-attachment: fixed;
 `
 
+const SkyImageComponent2 = styled.div`
+  width: 100%;
+  height: 100%;
+  background-image: url(${SkyImage});
+  z-index:-1;
+  position: fixed;
+  left: 0;
+  top:0;
+  background-size: cover;
+  background-attachment: fixed;
+`
+
 const Stacker = styled.div`
   position:relative;
 `
@@ -44,6 +57,26 @@ function progressToOpacity(progress: number) {
   return Math.min(1, 4.5 - progress * 5)
 }
 
+type InterpolateSettings = {
+  start: {
+    x: number,
+    y: number
+  }
+  end: {
+    x: number,
+    y: number
+  }
+}
+
+function interpolate(s: InterpolateSettings, progress: number): number {
+  const slope = (s.end.y - s.start.y) / (s.end.x - s.start.x)
+  const intercept = s.start.y - slope * s.start.x
+  const proposal = slope * progress + intercept
+  const min = Math.min(s.start.y, s.end.y)
+  const max = Math.max(s.start.y, s.end.y)
+  return Math.max(min, Math.min(max, proposal))
+}
+
 const WindowScene = () => {
   return (
     <Controller>
@@ -53,14 +86,39 @@ const WindowScene = () => {
         pin
       >
         {(progress: number) => (
-          <InteriorWall>
-            <Stacker style={{width: `${Math.max(20, 200 * progress - 40)}%`}}>
-              <Window shadePercentage={progressToShadePercentage(progress)} opacity={progressToOpacity(progress)}/>
+          <InteriorWall 
+            style={{
+              opacity:
+                interpolate({
+                  start: {x:0.66, y:1},
+                  end: {x:1, y:0},
+                }, progress)
+          }}
+          >
+            <Stacker 
+              style={{
+                width: `${
+                  interpolate({
+                    start: {x:0.33, y:25},
+                    end: {x:0.66, y:100},
+                  }, progress)
+                }%`
+              }}
+            >
+              <Window 
+                shadePercentage={
+                  interpolate({
+                    start: {x:0, y:60},
+                    end: {x:0.33, y:20},
+                  }, progress)
+                }
+              />
               <SkyImageComponent />
             </Stacker>
           </InteriorWall>
         )}
       </Scene>
+      <SkyImageComponent2 />
     </Controller>
   )
 }
