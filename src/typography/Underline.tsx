a6c7a78857668d218967229053ec74e4d34d4ebe
Sean Y <seanyang@college.harvard.edu>
2023-01-14 00:56:38 +0800

    Add Underline

---
+++ src/typography/Underline.tsx
@@ -0,0 +1,44 @@
+import Text from './Text';
+import styled from 'styled-components';
+import { font, fontSize } from '../constants/fonts';
+import color from '../constants/colors';
+
+const Underlined = styled.span`
+  text-decoration: underline;
+`;
+
+export type UnderlineProps = {
+  children: React.ReactNode,
+  overrideColor?: boolean,
+  overrideFont?: boolean,
+}
+
+const Underline = ({children, overrideColor=true, overrideFont=true}: UnderlineProps) => (
+  <Underlined>
+    {
+      overrideColor && overrideFont 
+      ?
+        <Text>
+          {children}
+        </Text>
+      :
+        overrideColor && !overrideFont
+        ?
+          <span style={{color: color.MAIN}}>
+            {children}
+          </span>
+        :
+          !overrideColor && overrideFont
+            ?
+            <span style={{fontFamily: font.BODY, fontSize: fontSize.BODY}}>
+              {children}
+            </span>
+            :
+            <span>
+              {children}
+            </span>
+    }
+  </Underlined>
+);
+
+export default Underline
