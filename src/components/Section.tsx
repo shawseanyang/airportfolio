51c7c11baec814988049479c5fbf4e12f958da5f
Sean Yang <53060248+shawseanyang@users.noreply.github.com>
2023-07-14 17:52:10 -0700

    remove email ticket

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -3,26 +3,19 @@ import SectionHeader, { SectionHeaderProps } from './SectionHeader'
 import Stack from 'react-bootstrap/Stack'
 import { forwardRef } from 'react'
 import Parser from '../typography/Parser'
-import EmailTicket, { EmailTicketProps, isEmailTicketProps } from './EmailTicket'
 
 export type SectionProps = {
   id: string;
-  tickets: (ParsableTicketProps | EmailTicketProps)[];
+  tickets: (ParsableTicketProps)[];
 } & SectionHeaderProps;
 
 const Section = forwardRef((props: SectionProps, ref) => (
   <Stack key={props.id} ref={ref as React.Ref<HTMLSpanElement>} id={props.id} gap={5} className={'my-4'}>
       <SectionHeader {...props}/>
       {props.tickets.map((ticket, index) => (
-          isEmailTicketProps(ticket) 
-          ? (
-            <EmailTicket key={index} {...ticket} />
-          )
-          : (
-            <Ticket {...ticket} key={index}>
-              <Parser text={ticket.text} />
-            </Ticket>
-          )
+        <Ticket {...ticket} key={index}>
+          <Parser text={ticket.text} />
+        </Ticket>
       ))}
     </Stack>
 ))
