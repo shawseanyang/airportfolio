993af0ecf8db9915dee31be80c0aabc489c30351
Sean Yang <53060248+shawseanyang@users.noreply.github.com>
2023-07-15 19:29:10 -0700

    fix key value pair spreading bug

--- src/components/Ticket/KeyValuePairs.tsx
+++ src/components/Ticket/KeyValuePairs.tsx
@@ -5,19 +5,34 @@ export type KeyValuePair = {
   value: string;
 }
 
-export type KeyValuePairsProps = KeyValuePair[];
+export type KeyValuePairsProps = {
+  pairs: KeyValuePair[];
+};
 
 // creates a stack of stacks, where the inner stacks have two elements, one for the key and one for the value
+// of props.length is 0, then render nothing
+// else render:
+// <Stack>
+//   {props.map((pair, index) => (
+//     <Stack key={index}>
+//       {/* TODO: change to typographic components */}
+//       <p>{pair.key}</p>
+//       <p>{pair.value}</p>
+//     </Stack>
+//   ))}
+// </Stack>
 const KeyValuePairs = (props: KeyValuePairsProps) => (
-  <Stack gap={1}>
-    {props.map((pair, index) => (
-      <Stack key={index} direction={'horizontal'} gap={1}>
-        {/* TODO: change to typographic components */}
-        <p>{pair.key}</p>
-        <p>{pair.value}</p>
-      </Stack>
-    ))}
-  </Stack>
+  props.pairs.length > 0
+    ? <Stack>
+      {props.pairs.map((pair, index) => (
+        <Stack key={index}>
+          {/* TODO: change to typographic components */}
+          <p>{pair.key}</p>
+          <p>{pair.value}</p>
+        </Stack>
+      ))}
+    </Stack>
+    : null
 );
 
 export default KeyValuePairs;
