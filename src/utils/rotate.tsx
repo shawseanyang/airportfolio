3b21c2c24c9ffe64b16ea9f3ac0278ab1a33b91f
Sean Yang <53060248+shawseanyang@users.noreply.github.com>
2023-07-25 16:53:05 -0700

    Reduce rotation variation

--- src/utils/rotate.tsx
+++ src/utils/rotate.tsx
@@ -1,6 +1,6 @@
 export function getRandomRotationDegree(): number {
-  const minDegree = -5;
-  const maxDegree = 5;
+  const minDegree = -3;
+  const maxDegree = 3;
 
   // Generate a random number between minDegree and maxDegree
   const randomDegree = Math.floor(Math.random() * (maxDegree - minDegree + 1)) + minDegree;
